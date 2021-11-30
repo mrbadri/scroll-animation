3a9610330f45f4ba82614cbddbed66a74d3ac4e3
MrBadri <badri0000badri@gmail.com>
2021-11-30 21:23:08 +0330

    create dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
 import { JWTProvider } from './contexts';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from './hook';
 
 function App() {
   const { logout } = useAuth();
+  const [drakMode, setdrakMode] = useState(true)
 
   useEffect(() => {
     console.log("test useeffect");
@@ -14,9 +15,12 @@ function App() {
 
   return (
     // <JWTProvider>
-      <div className="App flex justify-center items-center h-screen overflow-x-hidden overflow-y-auto">
-        <h1>Hello World!</h1>
-      </div>
+     <div className={drakMode && "dark"}>
+        <div className="App dark:bg-black dark:text-white flex justify-center items-center flex-col h-screen overflow-x-hidden overflow-y-auto">
+          <h1 className="mb-10" >Hello World!</h1>
+          <button className="bg-green-600 py-4 px-6 rounded dark:bg-green-400" onClick={()=>{setdrakMode(!drakMode)} }>change theme</button>
+        </div>
+     </div>
     // </JWTProvider>
   );
 }
