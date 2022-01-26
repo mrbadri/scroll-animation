74104c7a626547847dc70f1e3a56957558ea8c68
mohammad badri <mohammad.b@arianalabs.com>
2022-01-26 12:48:01 +0330

    add react frgment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React from "react";
+
+const Columns = () => (
+  <React.Fragment>
+    <th>Column1</th>
+    <th>Column2</th>
+    <th>Column3</th>
+    <th>Column4</th>
+  </React.Fragment>
+);
+
+// Description 
+// we can use <></> or <React.Fragment></React.Fragment>
+// if we want send props must use Fragment
+
 function App() {
   return (
     <div className="App flex justify-center items-center h-screen overflow-x-hidden overflow-y-auto">
-      <h1>Hello World!</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <Columns />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Row 1</td>
+              <td>Row 2</td>
+              <td>Row 3</td>
+              <td>Row 4</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
