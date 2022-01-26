fdd7b9e6aa98ff87ab5e99fbfa57443bad5067cd
mohammad badri <mohammad.b@arianalabs.com>
2022-01-26 11:47:34 +0330

    add error boudary file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ErrorBoundary>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>,
+  document.getElementById("root")
 );
